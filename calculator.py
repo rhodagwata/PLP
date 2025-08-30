a4ca0709e4aa711e6b7b2ccd8425f2e5d5152148
Dinah-Agwata <dinahbagwata@gmail.com>
2025-08-30 21:30:11 +0300

    Cleaning up the code

--- calculator.py
+++ calculator.py
@@ -1,9 +1,9 @@
-# Asking user for input
+
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Enter operation (+, -, *, /): ")
 
-# Perform the chosen operation
+
 if operation == "+":
     result = num1 + num2
     print(f"{num1} + {num2} = {result}")
